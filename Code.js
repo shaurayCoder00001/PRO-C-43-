96f24e536fd452aeb7e276bf0b6245c62b9c087f
shaurayCoder00001 <88592990+shaurayCoder00001@users.noreply.github.com>
2021-11-23 15:57:16 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "HAPPY";
+const accessCode2 = "ADDITON";
+const accessCode3 = "FOUR";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("Y P P A H", 100,50)
+    fill("lightblue")
+    text("Hint: Opposite of Sad !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text(" DDATOIN" ,700,150)
+    fill("lightblue")
+    text("Hint: This is a part of Bodmas !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("ROUF", 100,250)
+    fill("lightblue")
+    text("Hint: 2+2 is ", 100,270)
+
+}
